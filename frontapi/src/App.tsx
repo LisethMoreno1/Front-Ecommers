ba32ad2bd5ba5d0ba60351273bf6489226b95acc
LisethMoreno1 <lisethpatriciamorenomarrugo@gmail.com>
2024-10-16 16:39:24 -0500

    Estructuras de las carpetas

---
+++ frontapi/src/App.tsx
@@ -0,0 +1,12 @@
+import { BrowserRouter, BrowserRouter as Router } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ frontapi/src/components/products/productsPage.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function productsPage() {
+  return <div>productsPage</div>;
+}
